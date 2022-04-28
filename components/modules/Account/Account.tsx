3e329fac19279dd3b680e922ea5db784598742c6
Riddle <samuel.riddle1@gmail.com>
2022-04-27 22:42:38 -0600

    Implementation of useDApp feature

--- components/modules/Account/Account.tsx
+++ components/modules/Account/Account.tsx
@@ -4,95 +4,146 @@ import { injected } from "../../../connectors";
 import useENSName from "../../../hooks/useENSName";
 import useMetaMaskOnboarding from "../../../hooks/useMetaMaskOnboarding";
 import {formatEtherscanLink, parseBalance, shortenHex} from "../../../util";
-import {Button, Stack} from "@chakra-ui/react";
+import {Badge, Box, Button, SkeletonText, Text} from "@chakra-ui/react";
 import {FaWallet} from "react-icons/fa";
 import * as React from "react";
 import useONEBalance from "@/hooks/useONEBalance";
 import {Web3Provider} from "@ethersproject/providers";
 import useActiveWeb3React from "@/hooks/web3/use-active-web3-react";
+import { formatEther } from "@ethersproject/units";
+import {Harmony, useEtherBalance, useEthers} from "@usedapp/core";
 
 type AccountProps = {
   triedToEagerConnect: boolean;
 };
 
+// <div>
+//   {!account && <button >Connect</button>}
+//   {account && <p>Account: {account}</p>}
+//   {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+// </div>
+
 const Account = ({ triedToEagerConnect }: AccountProps) => {
-  const { active, error, activate, chainId, account, setError } = useActiveWeb3React();
-
-  const {
-    isMetaMaskInstalled,
-    isWeb3Available,
-    startOnboarding,
-    stopOnboarding,
-  } = useMetaMaskOnboarding();
-
-  // const { data: balance } = useONEBalance(account);
-  // const oneBalance = parseBalance(balance)
-  // manage connecting contexts for injected connector
-  const [connecting, setConnecting] = useState(false);
-  useEffect(() => {
-    if (active || error) {
-      setConnecting(false);
-      stopOnboarding();
-    }
-  }, [active, error, stopOnboarding]);
-
-  const ENSName = useENSName(account);
-
-  if (error || !triedToEagerConnect) {
-    return null;
-  }
-
+  const { activateBrowserWallet, account, chainId } = useEthers();
+  console.log(account)
+  const oneBalance = useEtherBalance(account, {chainId: Harmony.chainId})
   return (
-    <Stack pt={6}>
-      {isWeb3Available && !account ? (
-        <Button
-          leftIcon={<FaWallet />}
-          fontWeight={600}
-          bg="teal.400"
-          variant='solid'
-          disabled={connecting}
-          onClick={() => {
-            setConnecting(true);
-
-            activate(injected, undefined, true).catch((error) => {
-              // ignore the error if it's a user rejected request
-              if (error instanceof UserRejectedRequestError) {
-                setConnecting(false);
-              } else {
-                setError(error);
-              }
-            });
-          }}
-        >
-          Connect Wallet
-        </Button>
-      ) : (
+    <Box>
+      { account ? (
         <Button
           {...{
-            href: chainId != null && account != null ? formatEtherscanLink("Account", [chainId, account]) : '',
+            href: chainId != null && !!account ? formatEtherscanLink("Account", [chainId, account]) : '',
             target: "_blank",
             rel: "noopener noreferrer",
           }}
+          w={'100%'}
           leftIcon={<FaWallet />}
           fontWeight={600}
           variant="outline"
+          // bg="teal.500"
+        >
+
+          <Text>
+          {shortenHex(account, 3)}
+            <Badge colorScheme={'teal'} variant={'subtle'} borderRadius={'12px'} ml='1' fontSize='0.8em'>
+              {oneBalance ? `${parseBalance(oneBalance, 18, 2)} ONE` : <SkeletonText />}
+            </Badge>
+          </Text>
+        </Button>
+      ) : (
+        <Button
+          leftIcon={<FaWallet />}
+          fontWeight={600}
           _hover={{
             borderColor: "teal.400"
           }}
+          variant='solid'
+          onClick={() => activateBrowserWallet()}
         >
-          {/*{oneBalance}*/}
-          {ENSName ?? (account != null ? `${shortenHex(account, 4)}` : '')}
+          Connect Wallet
         </Button>
-        // <Button
-        //   onClick={startOnboarding}
-        //   bg="teal.400"
-        //   variant='solid'
-        // >
-        //   Install Metamask
-        // </Button>
       )}
-    </Stack>
-  );
+    </Box>
+  )
 }
+
+
+//   const { active, error, activate, chainId, account, setError } = useActiveWeb3React();
+//
+//   const {
+//     isMetaMaskInstalled,
+//     isWeb3Available,
+//     startOnboarding,
+//     stopOnboarding,
+//   } = useMetaMaskOnboarding();
+//
+//   // const { data: balance } = useONEBalance(account);
+//   // const oneBalance = parseBalance(balance)
+//   // manage connecting contexts for injected connector
+//   const [connecting, setConnecting] = useState(false);
+//   useEffect(() => {
+//     if (active || error) {
+//       setConnecting(false);
+//       stopOnboarding();
+//     }
+//   }, [active, error, stopOnboarding]);
+//
+//
+//   if (error || !triedToEagerConnect) {
+//     return null;
+//   }
+//
+//   return (
+//     <Stack pt={6}>
+//       {isWeb3Available && !account ? (
+//         <Button
+//           leftIcon={<FaWallet />}
+//           fontWeight={600}
+//           bg="teal.400"
+//           variant='solid'
+//           disabled={connecting}
+//           onClick={() => {
+//             setConnecting(true);
+//
+//             activate(injected, undefined, true).catch((error) => {
+//               // ignore the error if it's a user rejected request
+//               if (error instanceof UserRejectedRequestError) {
+//                 setConnecting(false);
+//               } else {
+//                 setError(error);
+//               }
+//             });
+//           }}
+//         >
+//           Connect Wallet
+//         </Button>
+//       ) : (
+//         <Button
+//           {...{
+//             href: chainId != null && account != null ? formatEtherscanLink("Account", [chainId, account]) : '',
+//             target: "_blank",
+//             rel: "noopener noreferrer",
+//           }}
+//           leftIcon={<FaWallet />}
+//           fontWeight={600}
+//           variant="outline"
+//           _hover={{
+//             borderColor: "teal.400"
+//           }}
+//         >
+//           {/*{oneBalance}*/}
+//           {ENSName ?? (account != null ? `${shortenHex(account, 4)}` : '')}
+//         </Button>
+//         // <Button
+//         //   onClick={startOnboarding}
+//         //   bg="teal.400"
+//         //   variant='solid'
+//         // >
+//         //   Install Metamask
+//         // </Button>
+//       )}
+//     </Stack>
+//   );
+// }
 
 export default Account;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,27 @@ import "../styles/globals.css";
 import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 import mainTheme from '../theme'
 import Layout from "@/components/layouts/Layout";
+import {Config, Harmony, DAppProvider} from "@usedapp/core";
+import {getDefaultProvider} from "ethers";
+import {ReactNode} from "react";
 
-function NextWeb3App({ Component, pageProps }: AppProps) {
+const config: Config = {
+  readOnlyChainId: Harmony.chainId,
+  readOnlyUrls: {
+    [Harmony.chainId]: "https://rpc.foxswap.fi"
+  }
+}
+
+function App({ Component, pageProps }: AppProps) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
+    <DAppProvider config={config}>
       <ChakraProvider resetCSS theme={mainTheme}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
-    </Web3ReactProvider>
+    </DAppProvider>
   );
 }
 
-export default NextWeb3App;
+export default App;
